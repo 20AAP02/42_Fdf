934d5ec7b9fde77669c41825c9503b9977c42005
20AAP02 <antoniomariabranches@gmail.com>
2022-04-05 14:57:31 +0100

    added libft

---
+++ fdf_dir/42_libft/ft_atoi.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amaria-m <amaria-m@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/05 11:07:31 by amaria-m          #+#    #+#             */
+/*   Updated: 2022/04/05 12:25:08 by amaria-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+#include "../ft_printf/ft_printf.h"
+
+int	ft_atoi(const char *str)
+{
+	int	number;
+	int	sign;
+
+	sign = 1;
+	number = 0;
+	while (*str == ' ' || *str == '\f' || *str == '\n'
+		|| *str == '\r' || *str == '\t' || *str == '\v')
+		str++;
+	if (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			sign *= -1;
+		str++;
+	}
+	while (*str > 47 && *str < 58)
+	{
+		number = (number * 10) + (*str - 48);
+		str++;
+	}
+	return (number * sign);
+}
---
+++ fdf_dir/main.c
@@ -0,0 +1,8 @@
+#include "42_libft/libft.h"
+#include "ft_printf/ft_printf.h"
+
+int main()
+{
+    ft_printf("hello world\n");
+    return 0;
+}
