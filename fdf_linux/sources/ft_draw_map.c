153368e81691bc57eabed8c2a57e74e502e88a68
20AAP02 <antoniomariabranches@gmail.com>
2022-04-21 11:57:54 +0100

    map files changed to fdf_linux and i can read all files

--- fdf_linux/sources/ft_draw_map.c
+++ fdf_linux/sources/ft_draw_map.c
@@ -16,21 +16,23 @@ void	ft_draw_map(void *mlx, t_img *img, int color, t_map *map_inf)
 {
 	int	pt_dist;
 	int	pixel;
-	int	img_center;
+	int	img_Ycenter;
+	int	img_Xcenter;
 	int	x;
 	int	y;
 
 	if (img->pixel_bits != 32)
 		color = mlx_get_color_value(mlx, color);
 	pt_dist = ft_calculate_pt_dist(map_inf->width, img->width, map_inf->height, img->height);
-	img_center = (img->height - (map_inf->height * pt_dist)) / 2;
+	img_Ycenter = (img->height - (map_inf->height * pt_dist)) / 2;
+	img_Xcenter = (img->width - (map_inf->width * pt_dist));
 	y = 0;
 	while (map_inf->map[y])
 	{
 		x = 0;
 		while (x < map_inf->width)
 		{
-			pixel = ((y * pt_dist + img_center) * img->line_bytes) + ((x * pt_dist) * 4);
+			pixel = (((y * pt_dist) + img_Ycenter) * img->line_bytes) + (((x * pt_dist) + img_Xcenter) * 4);
 			ft_color_img_pixel(img, pixel, color * map_inf->map[y][x]);
 			x++;
 		}
