566da88dc117ced7225f10d2e7b36c3204a8a427
20AAP02 <antoniomariabranches@gmail.com>
2022-04-26 14:48:41 +0100

    resize is working for all pictures, still have to add to option to enlarge img

--- fdf_linux/sources/ft_draw_lines.c
+++ fdf_linux/sources/ft_draw_lines.c
@@ -14,40 +14,82 @@ int iso(int x, int y, int z, int check)
     return (x);
 }
 
-t_pt ft_lowest_pt(t_map *map_inf, int comp)
+int ft_lowest_y(t_map *map_inf, int comp)
 {
     int     y;
     int     x;
     int     z;
-    t_pt    a;
-    t_pt    low_pt;
+    int     y_3d;
+    int     low_y;
 
     y = 0;
+    low_y = 0;
     while (y < map_inf->height)
     {
         x = 0;
         while (x < map_inf->width)
         {
             z = map_inf->map[y][x].altitude;
-            a.x = iso(x, y, z, 0) + comp;
-            a.y = iso(x, y, z, 1) + comp;
-            if (x == 0 && y == 0)
-                low_pt = a;
-            else if ((low_pt.x > a.x && low_pt.y > a.x) || (low_pt.x > a.y && low_pt.y > a.y))
-                low_pt = a;
+            y_3d = iso(x, y, z, 1) + comp;
+            if (y == 0)
+                low_y = y_3d;
+            else if (low_y > y_3d)
+                low_y = y_3d;
             x++;
         }
         y++;
     }
-    return (low_pt);
+    return (low_y);
 }
 
-int ft_pt_compensation(t_map *map_inf)
+int ft_lowest_x(t_map *map_inf, int comp)
+{
+    int     y;
+    int     x;
+    int     z;
+    int     x_3d;
+    int     low_x;
+
+    y = 0;
+    low_x = 0;
+    while (y < map_inf->height)
+    {
+        x = 0;
+        while (x < map_inf->width)
+        {
+            z = map_inf->map[y][x].altitude;
+            x_3d = iso(x, y, z, 0) + comp;
+            if (y == 0)
+                low_x = x_3d;
+            else if (low_x > x_3d)
+                low_x = x_3d;
+            x++;
+        }
+        y++;
+    }
+    return (low_x);
+}
+
+int ft_pt_compensate_x(t_map *map_inf)
 {
     int comp;
 
-    comp = 1;
-    while (ft_lowest_pt(map_inf, comp).x < 0 || ft_lowest_pt(map_inf, comp).y < 0)
+    comp = 0;
+    while (ft_lowest_x(map_inf, comp) < 0)
+    {
+        printf("comp - %i\n", comp);
+        comp++;
+    }
+
+    return (comp);
+}
+
+int ft_pt_compensate_y(t_map *map_inf)
+{
+    int comp;
+
+    comp = 0;
+    while (ft_lowest_y(map_inf, comp) < 0)
     {
         printf("comp - %i\n", comp);
         comp++;
@@ -66,9 +108,11 @@ void    ft_draw_H_lines(void *mlx, t_img *img, t_map *map_inf)
     t_pt    a;
     t_pt    b;
     int     z;
-    int     comp;
+    int     comp_x;
+    int     comp_y;
 
-    comp = ft_pt_compensation(map_inf);
+    comp_x = ft_pt_compensate_x(map_inf);
+    comp_y = ft_pt_compensate_y(map_inf);
     y = 0;
     while (y < map_inf->height)
     {
@@ -76,9 +120,9 @@ void    ft_draw_H_lines(void *mlx, t_img *img, t_map *map_inf)
         while (x < map_inf->width - 1)
         {
             z = map_inf->map[y][x].altitude;
-            a.x = iso(x, y, z, 0) + comp;
-            a.y = iso(x, y, z, 1) + comp;
-            b.x = iso(x + 1, y, z, 0) + comp;
+            a.x = iso(x, y, z, 0) + comp_x;
+            a.y = iso(x, y, z, 1) + comp_y;
+            b.x = iso(x + 1, y, z, 0) + comp_x;
             line = ft_init_line(a.x, a.y, b.x, a.y);
             line.colors[0] = map_inf->map[y][x].color;
             line.colors[1] = map_inf->map[y][x + 1].color;
@@ -99,9 +143,11 @@ void    ft_draw_V_lines(void *mlx, t_img *img, t_map *map_inf)
     t_pt    a;
     t_pt    b;
     int     z;
-    int     comp;
+    int     comp_x;
+    int     comp_y;
 
-    comp = ft_pt_compensation(map_inf);
+    comp_x = ft_pt_compensate_x(map_inf);
+    comp_y = ft_pt_compensate_y(map_inf);
     y = 0;
     while (y < map_inf->height - 1)
     {
@@ -109,9 +155,9 @@ void    ft_draw_V_lines(void *mlx, t_img *img, t_map *map_inf)
         while (x < map_inf->width)
         {
             z = map_inf->map[y][x].altitude;
-            a.x = iso(x, y, z, 0) + comp;
-            a.y = iso(x, y, z, 1) + comp;
-            b.y = iso(x, y + 1, z, 1) + comp;
+            a.x = iso(x, y, z, 0) + comp_x;
+            a.y = iso(x, y, z, 1) + comp_y;
+            b.y = iso(x, y + 1, z, 1) + comp_y;
             line = ft_init_line(a.x, a.y, a.x, b.y);
             line.colors[0] = map_inf->map[y][x].color;
             line.colors[1] = map_inf->map[y + 1][x].color;
