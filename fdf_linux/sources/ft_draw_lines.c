8e2c4cf2b7fb7bb3bbf91c69b5d61cc230a225ea
20AAP02 <antoniomariabranches@gmail.com>
2022-04-26 13:44:12 +0100

    working on resizing of the isometric image

--- fdf_linux/sources/ft_draw_lines.c
+++ fdf_linux/sources/ft_draw_lines.c
@@ -14,6 +14,48 @@ int iso(int x, int y, int z, int check)
     return (x);
 }
 
+t_pt ft_lowest_pt(t_map *map_inf, int comp)
+{
+    int     y;
+    int     x;
+    int     z;
+    t_pt    a;
+    t_pt    low_pt;
+
+    y = 0;
+    while (y < map_inf->height)
+    {
+        x = 0;
+        while (x < map_inf->width)
+        {
+            z = map_inf->map[y][x].altitude;
+            a.x = iso(x, y, z, 0) + comp;
+            a.y = iso(x, y, z, 1) + comp;
+            if (x == 0 && y == 0)
+                low_pt = a;
+            else if ((low_pt.x > a.x && low_pt.y > a.x) || (low_pt.x > a.y && low_pt.y > a.y))
+                low_pt = a;
+            x++;
+        }
+        y++;
+    }
+    return (low_pt);
+}
+
+int ft_pt_compensation(t_map *map_inf)
+{
+    int comp;
+
+    comp = 1;
+    while (ft_lowest_pt(map_inf, comp).x < 0 || ft_lowest_pt(map_inf, comp).y < 0)
+    {
+        printf("comp - %i\n", comp);
+        comp++;
+    }
+
+    return (comp);
+}
+
 
 
 void    ft_draw_H_lines(void *mlx, t_img *img, t_map *map_inf)
@@ -24,10 +66,9 @@ void    ft_draw_H_lines(void *mlx, t_img *img, t_map *map_inf)
     t_pt    a;
     t_pt    b;
     int     z;
-    int     range;
+    int     comp;
 
-    map_inf->pt_dist = ft_calculate_pt_dist(map_inf->width, img->width, map_inf->height, img->height);
-    map_inf->pt_dist = 1;
+    comp = ft_pt_compensation(map_inf);
     y = 0;
     while (y < map_inf->height)
     {
@@ -35,15 +76,15 @@ void    ft_draw_H_lines(void *mlx, t_img *img, t_map *map_inf)
         while (x < map_inf->width - 1)
         {
             z = map_inf->map[y][x].altitude;
-            range = map_inf->range[1] - map_inf->range[0];
-            a.x = (iso(x, y, z, 0) * map_inf->pt_dist) + (map_inf->pt_dist * range);
-            a.y = (iso(x, y, z, 1) * map_inf->pt_dist) + (map_inf->pt_dist * range);
-            b.x = (iso(x + 1, y, z, 0) * map_inf->pt_dist) + (map_inf->pt_dist * range);
+            a.x = iso(x, y, z, 0) + comp;
+            a.y = iso(x, y, z, 1) + comp;
+            b.x = iso(x + 1, y, z, 0) + comp;
             line = ft_init_line(a.x, a.y, b.x, a.y);
             line.colors[0] = map_inf->map[y][x].color;
             line.colors[1] = map_inf->map[y][x + 1].color;
             if (a.x < img->width && b.x < img->width && a.y < img->height)
-                ft_draw_line(mlx, img, line);
+                if (a.x >= 0 && b.x >= 0 && a.y >= 0)
+                    ft_draw_line(mlx, img, line);
             x++;
         }
         y++;
@@ -58,10 +99,9 @@ void    ft_draw_V_lines(void *mlx, t_img *img, t_map *map_inf)
     t_pt    a;
     t_pt    b;
     int     z;
-    int     range;
+    int     comp;
 
-    map_inf->pt_dist = ft_calculate_pt_dist(map_inf->width, img->width, map_inf->height, img->height);
-    map_inf->pt_dist = 1;
+    comp = ft_pt_compensation(map_inf);
     y = 0;
     while (y < map_inf->height - 1)
     {
@@ -69,15 +109,15 @@ void    ft_draw_V_lines(void *mlx, t_img *img, t_map *map_inf)
         while (x < map_inf->width)
         {
             z = map_inf->map[y][x].altitude;
-            range = map_inf->range[1] - map_inf->range[0];
-            a.x = (iso(x, y, z, 0) * map_inf->pt_dist) + (map_inf->pt_dist * range);
-            a.y = (iso(x, y, z, 1) * map_inf->pt_dist) + (map_inf->pt_dist * range);
-            b.y = (iso(x, y + 1, z, 1) * map_inf->pt_dist) + (map_inf->pt_dist * range);
+            a.x = iso(x, y, z, 0) + comp;
+            a.y = iso(x, y, z, 1) + comp;
+            b.y = iso(x, y + 1, z, 1) + comp;
             line = ft_init_line(a.x, a.y, a.x, b.y);
             line.colors[0] = map_inf->map[y][x].color;
             line.colors[1] = map_inf->map[y + 1][x].color;
             if (a.x < img->width && a.y < img->height && b.y < img->height)
-                ft_draw_line(mlx, img, line);
+                if (a.x >= 0 && a.y >= 0 && b.y >= 0)
+                    ft_draw_line(mlx, img, line);
             x++;
         }
         y++;
